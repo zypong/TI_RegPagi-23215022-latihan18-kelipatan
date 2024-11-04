5dba3ec3cf74873bedbf540781673787456704bb
zypong <imzyzulijar01@gmail.com>
2024-11-04 11:52:23 +0700

    Add files via upload

---
+++ kelipatan.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan5;
+
+/**
+ *NAMA  : IMZY ZULIJAR SETIAWAN
+ *NIM   : 23215022
+ *PRODI : TEKNIK INFORMATIKA
+ * 
+ * menampilkan bilangan kelipatan
+ */
+public class kelipatan {
+    public static void main(String[] args) {
+        // Mulai dari 3.5, kelipatan 3.5
+        double angka = 3.5;
+        
+        // Looping dari 3.5 sampai dengan 35.0
+        while (angka <= 35.0) {
+            System.out.println(angka);
+            angka += 3.5; // Tambahkan 3.5 setiap iterasi
+        }
+    }
+    }
